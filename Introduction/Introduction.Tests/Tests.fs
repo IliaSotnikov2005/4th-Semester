c4e2cc586d2dec9903adfa4c49ff6b4e61b1a35a
Ilya Sotnikov <ilia12st@yandex.ru>
2025-02-20 12:29:57 +0300

    upload introduction tasks

---
+++ Introduction/Introduction.Tests/Tests.fs
@@ -0,0 +1,112 @@
+﻿module Introduction.Tests
+
+open NUnit.Framework
+open Introduction.Functions
+
+[<TestFixture>]
+type FactorialTests() =
+
+    [<Test>]
+    member this.``Factorial of negative number should return None``() =
+        let result = factorial -3
+        Assert.That(None, Is.EqualTo(result))
+
+    [<Test>]
+    member this.``Factorial of 0 should return Some 1``() =
+        let result = factorial 0
+        Assert.That(Some 1, Is.EqualTo(result))
+
+    [<Test>]
+    member this.``Factorial of 5 should return Some 120``() =
+        let result = factorial 5
+        Assert.That(Some 120, Is.EqualTo(result))
+
+    [<Test>]
+    member this.``Factorial of 3 should return Some 6``() =
+        let result = factorial 3
+        Assert.That(Some 6, Is.EqualTo(result))
+
+[<TestFixture>]
+type FibonacciTests() =
+    [<Test>]
+    member this.``Fibonacci of negative number should return None``() =
+        let result = fibonacci -4
+        Assert.That(None, Is.EqualTo(result))
+
+    [<Test>]
+    member this.``Fibonacci of 6 should return Some 8``() =
+        let result = fibonacci 6
+        Assert.That(Some 8, Is.EqualTo(result))
+
+[<TestFixture>]
+type ReverseTests() =
+
+    [<Test>]
+    member this.``Reverse of empty list should return empty list``() =
+        let result = reverse []
+        Assert.That(result, Is.EqualTo([]))
+
+    [<Test>]
+    member this.``Reverse of single-element list should return the same list``() =
+        let result = reverse [1]
+        Assert.That(result, Is.EqualTo([1]))
+
+    [<Test>]
+    member this.``Reverse of multiple-element list should return reversed list``() =
+        let result = reverse [1; 2; 3; 4]
+        Assert.That(result, Is.EqualTo([4; 3; 2; 1]))
+
+    [<Test>]
+    member this.``Reverse of list with duplicate elements should return reversed list``() =
+        let result = reverse [1; 2; 2; 3]
+        Assert.That(result, Is.EqualTo([3; 2; 2; 1]))
+
+[<TestFixture>]
+type FindTests() =
+
+    [<Test>]
+    member this.``Find in empty list should return None``() =
+        let result = find 1 []
+        Assert.That(result, Is.EqualTo(None))
+
+    [<Test>]
+    member this.``Find in single-element list should return 0``() =
+        let result = find 1 [1]
+        Assert.That(result, Is.EqualTo(Some 0))
+
+    [<Test>]
+    member this.``Find in multiple-element list should return index of element``() =
+        let result = find 3 [1; 2; 3; 4]
+        Assert.That(result, Is.EqualTo(Some 2))
+
+    [<Test>]
+    member this.``Find in list with duplicate elements should return index of first occurance``() =
+        let result = find 2 [1; 2; 2; 3]
+        Assert.That(result, Is.EqualTo(Some 1))
+
+    [<Test>]
+    member this.``Find in list without element should return None``() =
+        let result = find 4 [1; 2; 2; 3]
+        Assert.That(result, Is.EqualTo(None))
+
+[<TestFixture>]
+type getListOfpowersOfTwoTests() =
+    [<Test>]
+    member this.``Get list with m < n should return empty list``() =
+        let result = getListOfpowersOfTwo 5 1
+        Assert.That(result, Is.EqualTo([] : double list))
+
+    [<Test>]
+    member this.``Get list with n <= m and n, m > 0 should return correct list``() =
+        let result = getListOfpowersOfTwo 1 5
+        Assert.That(result, Is.EqualTo([2.0; 4.0; 8.0; 16.0; 32.0]))
+
+    [<Test>]
+    member this.``Get list with n <= m and n, m < 0 should return correct list``() =
+        let result = getListOfpowersOfTwo -3 -1
+        Assert.That(result, Is.EqualTo([0.125; 0.25; 0.5]))
+    
+    [<Test>]
+    member this.``Get list with n <= m and n < 0 and m > 0 should return correct list``() =
+        let result = getListOfpowersOfTwo -3 3
+        Assert.That(result, Is.EqualTo([0.125; 0.25; 0.5; 1.0; 2.0; 4.0; 8.0]))
---
+++ Introduction/Introduction/Program.fs
@@ -0,0 +1,37 @@
+﻿namespace Introduction
+
+module Functions =
+    let factorial n =
+        if n < 0 then None 
+        elif n = 0 then Some 1
+        else Some (Seq.reduce (*) {1 .. n})
+
+    let rec fibonacci n =
+        if n < 1 then None
+        elif n = 1 || n = 2 then Some 1
+        else Some (fibonacci(n - 1).Value + fibonacci(n - 2).Value)
+
+    let reverse lst =
+        let rec reverser acc lst =
+            match lst with
+            | [] -> acc
+            | head :: tail -> reverser (head :: acc) tail
+        reverser [] lst
+
+    let find n lst =
+        let rec finder acc n lst=
+            match lst with
+            | [] -> None
+            | head :: tail -> if head = n then Some acc else finder (acc + 1) n tail
+        finder 0 n lst
+
+    let getListOfpowersOfTwo n m =
+        if m >= n then
+            let rec generatePowers current power =
+                if power > m then
+                    []
+                else
+                    current :: generatePowers (current * 2.0) (power + 1)
+            generatePowers (pown 2.0 n) n
+        else
+            []
