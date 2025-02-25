e9a3444756a01b0adcf82bb35cd0652a3a96f25c
Ilya Sotnikov <ilia12st@yandex.ru>
2025-02-25 16:33:06 +0300

    upload task

---
+++ PrimeNumbers/PrimeNumbers.Tests/Tests.fs
@@ -0,0 +1,41 @@
+﻿module Tests
+
+open Xunit
+open FsUnit.Xunit
+open PrimeNumbers
+
+[<Fact>]
+let ``isPrime should return false for numbers less than 2`` () =
+    isPrime 0 |> should be False
+    isPrime 1 |> should be False
+
+[<Fact>]
+let ``isPrime should return true for prime numbers`` () =
+    isPrime 2 |> should be True
+    isPrime 3 |> should be True
+    isPrime 5 |> should be True
+    isPrime 7 |> should be True
+    isPrime 11 |> should be True
+
+[<Fact>]
+let ``isPrime should return false for non-prime numbers`` () =
+    isPrime 4 |> should be False
+    isPrime 6 |> should be False
+    isPrime 8 |> should be False
+    isPrime 9 |> should be False
+    isPrime 10 |> should be False
+
+[<Fact>]
+let ``primeNumbers should generate the first 5 prime numbers`` () =
+    primeNumbers
+    |> Seq.take 5
+    |> Seq.toList
+    |> should equal [2; 3; 5; 7; 11]
+
+[<Fact>]
+let ``primeNumbers should generate primes greater than 100`` () =
+    primeNumbers
+    |> Seq.skipWhile (fun x -> x <= 100)
+    |> Seq.take 5
+    |> Seq.toList
+    |> should equal [101; 103; 107; 109; 113]
---
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.fs
@@ -0,0 +1,14 @@
+﻿module PrimeNumbers
+
+let isPrime n =
+    if n < 2 then false
+    else
+        let sqrtN = int (sqrt (float n))
+        { 2 .. sqrtN } |> Seq.forall (fun x -> n % x <> 0)
+
+let primeNumbers =
+    let rec nextPrime n =
+        if isPrime n then n
+        else nextPrime (n + 1)
+    
+    Seq.unfold (fun n -> Some(n, nextPrime (n + 1))) 2
