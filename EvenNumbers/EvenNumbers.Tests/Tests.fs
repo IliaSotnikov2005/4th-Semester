bb6a272af63075e3e4a1370df167187747c60860
Ilya Sotnikov <ilia12st@yandex.ru>
2025-02-25 14:49:42 +0300

    upload task

---
+++ EvenNumbers/EvenNumbers.Tests/Tests.fs
@@ -0,0 +1,34 @@
+﻿module Tests
+
+open Xunit
+open FsUnit.Xunit
+open EvenNumbersCounter
+open FsCheck.Xunit
+
+[<Property>]
+let ``All implementations should be equivalent`` (list: int list) =
+    let resultMap = mapRealisation list
+    let resultFilter = filterRealisation list
+    let resultFold = foldRealisation list
+    resultMap = resultFilter && resultFilter = resultFold
+
+let implementations = [
+    mapRealisation
+    filterRealisation
+    foldRealisation
+]
+
+let testData = [
+    ([], 0)
+    ([1; 2; 3; 4; 5], 2)
+    ([2; 4; 6; 8], 4)
+    ([1; 3; 5; 7], 0)
+    ([0; -2; -3; -4], 3)
+]
+
+[<Theory>]
+[<MemberData(nameof(testData))>]
+let ``All implementations should count even numbers correctly`` (list: int list, expected: int) =
+    for implementation in implementations do
+        let result = implementation list
+        result |> should equal expected
---
+++ EvenNumbers/EvenNumbers/EvenNumbersCounter.fs
@@ -0,0 +1,9 @@
+﻿module EvenNumbersCounter
+    let mapRealisation list =
+        list |> List.map (fun element -> if element % 2 = 0 then 1 else 0) |> List.sum
+
+    let filterRealisation list =
+        list |> List.filter (fun element -> element % 2 = 0) |> List.length
+    
+    let foldRealisation list =
+        list |> List.fold (fun acc element -> if element % 2 = 0 then acc + 1 else acc) 0
