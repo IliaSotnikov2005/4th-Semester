09b08a94cf8d9d7b95c4d3c4be9eebb4274b2470
Ilya Sotnikov <ilia12st@yandex.ru>
2025-03-01 12:13:28 +0300

    upload task

---
+++ MapForTrees/MapForTrees/Library.fs
@@ -0,0 +1,17 @@
+﻿module MapForTrees
+
+type BinTree<'a> =
+    | Empty
+    | Node of 'a * BinTree<'a> * BinTree<'a>
+
+let map f binTree =
+    let rec mapStep tree cont =
+        match tree with
+        | Empty -> cont Empty
+        | Node(value, left, right) ->
+            let newValue = f value
+            mapStep left (fun newLeft ->
+                mapStep right (fun newRight ->
+                    cont (Node(newValue, newLeft, newRight))))
+
+    mapStep binTree id
