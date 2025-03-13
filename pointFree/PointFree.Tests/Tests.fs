e8788772336c603cf6a7c7e521bd3b8c59db4865
Ilya Sotnikov <ilia12st@yandex.ru>
2025-03-13 14:32:22 +0300

    upload

---
+++ pointFree/PointFree.Tests/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open FsCheck
+open FsCheck.Xunit
+open PointFree
+
+[<Property>]
+let ``All functions are equivalent`` (x : int) (l : int list) =
+    let result1 = originalFunction x l
+    let result2 = transformedFunction1 x l
+    let result3 = transformedFunction2 x l
+    let result4 = transformedFunction3 x l
+    
+    result1 = result2 && result2 = result3 && result3 = result4
---
+++ pointFree/PointFree/PointFree.fs
@@ -0,0 +1,7 @@
+﻿module PointFree
+
+let originalFunction x l = List.map (fun y -> y * x) l
+
+let transformedFunction1 x = List.map (fun y -> y * x)
+let transformedFunction2 x = List.map((*) x)
+let transformedFunction3 = List.map << (*)
