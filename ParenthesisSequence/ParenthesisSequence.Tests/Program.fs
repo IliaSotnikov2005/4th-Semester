fea1ae66b190f624e63c39648fee06ec528d1b19
Ilya Sotnikov <ilia12st@yandex.ru>
2025-03-13 10:54:52 +0300

    upload

---
+++ ParenthesisSequence/ParenthesisSequence.Tests/Program.fs
@@ -0,0 +1,49 @@
+﻿module ParenthesisSequence.Tests
+
+open Xunit
+open FsUnit.Xunit
+open ParenthesisSequence
+
+[<Fact>]
+let ``Empty string should be balanced`` () =
+    checkBracketBalance "" |> should be True
+
+[<Fact>]
+let ``Single open bracket should be unbalanced`` () =
+    checkBracketBalance "(" |> should be False
+
+[<Fact>]
+let ``Single close bracket should be unbalanced`` () =
+    checkBracketBalance ")" |> should be False
+
+[<Fact>]
+let ``Balanced parentheses should return true`` () =
+    checkBracketBalance "()" |> should be True
+
+[<Fact>]
+let ``Balanced mixed brackets should return true`` () =
+    checkBracketBalance "({[]})" |> should be True
+
+[<Fact>]
+let ``Unbalanced mixed brackets should return false`` () =
+    checkBracketBalance "({[})" |> should be False
+
+[<Fact>]
+let ``Nested unbalanced brackets should return false`` () =
+    checkBracketBalance "({[}])" |> should be False
+
+[<Fact>]
+let ``Balanced string with non-bracket characters should ignore them`` () =
+    checkBracketBalance "(a{bdafdf}aac[dfdfeed]edfdfadf)" |> should be True
+
+[<Fact>]
+let ``Unbalanced string with non-bracket characters should return false`` () =
+    checkBracketBalance "(afsdaf{bd}cdddd[d}e)" |> should be False
+
+[<Fact>]
+let ``Complex balanced string should return true`` () =
+    checkBracketBalance "[]{{}()}({[()]})" |> should be True
+
+[<Fact>]
+let ``Complex unbalanced string should return false`` () =
+    checkBracketBalance "[]{{}()}({[()]}))" |> should be False
---
+++ ParenthesisSequence/ParenthesisSequence/ParenthesisSequence.fs
@@ -0,0 +1,30 @@
+﻿module ParenthesisSequence
+
+let getPair bracket =
+    match bracket with
+    | '(' -> ')'
+    | '[' -> ']'
+    | '{' -> '}'
+    | _ -> failwith "Invalid bracket"
+
+let isOpenBracket bracket = bracket = '(' || bracket = '[' || bracket = '{'
+let isCloseBracket bracket = bracket = ')' || bracket = ']' || bracket = '}'
+
+let rec checkBracketBalance (str: string) =
+    let rec check chars (stack: char list) =
+        match chars with
+        | [] -> stack.IsEmpty
+        | head :: tail ->
+            if isOpenBracket head then
+                check tail (head :: stack)
+            elif isCloseBracket head then
+                match stack with
+                | [] -> false
+                | top :: stackTail ->
+                    if head = getPair top then
+                        check tail stackTail
+                    else
+                        false
+            else
+                check tail stack
+    check (List.ofSeq str) []
