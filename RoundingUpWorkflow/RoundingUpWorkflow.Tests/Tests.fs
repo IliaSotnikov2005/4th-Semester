1ab600776b3f40cf46805037a380442193d3e7c7
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-05 13:09:25 +0300

    upload task

---
+++ RoundingUpWorkflow/RoundingUpWorkflow.Tests/Tests.fs
@@ -0,0 +1,37 @@
+﻿module Tests
+
+open Xunit
+open FsUnit.Xunit
+open RoundingUpWorkflow
+
+[<Fact>]
+let ``Rounding workflow should round intermediate steps`` () =
+    let result = 
+        RoundingUpBuilder 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+    
+    result |> should (equalWithin 0.001) 0.048
+
+[<Fact>]
+let ``Rounding workflow should round initial values`` () =
+    let result =
+        RoundingUpBuilder 2 {
+            let! a = 2.335
+            return a
+        }
+    
+    result |> should (equalWithin 0.001) 2.34
+
+[<Fact>]
+let ``Should not round exact values`` () =
+    let result = 
+        RoundingUpBuilder 4 {
+            let! a = 2.5
+            let! b = 1.0000
+            return a + b
+        }
+    
+    result |> should (equalWithin 0.0001) 3.5
---
+++ RoundingUpWorkflow/RoundingUpWorkflow/RoundingUpWorkflow.fs
@@ -0,0 +1,7 @@
+﻿module RoundingUpWorkflow
+
+type RoundingUpBuilder(digits: int) =
+    member this.Bind(x: float, f: float -> float) =
+        System.Math.Round(x, digits) |> f
+
+    member this.Return(x: float) = System.Math.Round(x, digits)
