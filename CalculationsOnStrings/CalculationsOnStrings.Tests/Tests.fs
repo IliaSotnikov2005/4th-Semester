fb779b22392a363bb72d80c9b46118971062e56d
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-05 13:38:40 +0300

    upload task

---
+++ CalculationsOnStrings/CalculationsOnStrings.Tests/Tests.fs
@@ -0,0 +1,41 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+open CalculationsOnStrings
+
+[<Test>]
+let ``Valid numbers should return sum`` () =
+    let result = calculator {
+        let! a = "121"
+        let! b = "14"
+        return a + b
+    }
+    result |> should equal (Some 135)
+
+[<Test>]
+let ``Invalid first number should return None`` () =
+    let result = calculator {
+        let! a = "abc"
+        let! b = "14"
+        return a + b
+    }
+    result |> should equal None
+
+[<Test>]
+let ``Invalid second number should return None`` () =
+    let result = calculator {
+        let! a = "121"
+        let! b = "1x4"
+        return a + b
+    }
+    result |> should equal None
+
+[<Test>]
+let ``Both invalid numbers should return None`` () =
+    let result = calculator {
+        let! a = "12.5"
+        let! b = "14z"
+        return a + b
+    }
+    result |> should equal None
---
+++ CalculationsOnStrings/CalculationsOnStrings/CalculationsOnStrings.fs
@@ -0,0 +1,12 @@
+﻿module CalculationsOnStrings
+
+type CalculatorOnStringsBuilder() =
+    member this.Bind(x: string, f: int -> int option) =
+        match System.Int32.TryParse x with
+        | true, num -> f num
+        | false, _ -> None
+
+    member this.Return(x: int) =
+        Some x
+
+let calculator = CalculatorOnStringsBuilder()
