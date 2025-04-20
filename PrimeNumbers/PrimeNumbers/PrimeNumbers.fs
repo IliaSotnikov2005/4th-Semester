42a0a36e51a05d33f24dd3790621e5730e2a9e0d
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-20 15:38:20 +0300

    add documentation

--- PrimeNumbers/PrimeNumbers/PrimeNumbers.fs
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.fs
@@ -1,11 +1,14 @@
-﻿module PrimeNumbers
+﻿/// A module containing a prime number generator.
+module PrimeNumbers
 
+/// Checks whether a number is prime.
 let isPrime n =
     if n < 2 then false
     else
         let sqrtN = int (sqrt (float n))
         { 2 .. sqrtN } |> Seq.forall (fun x -> n % x <> 0)
 
+/// Generates a prime numbers sequence.
 let primeNumbers =
     let rec nextPrime n =
         if isPrime n then n
