d40a3e6a8c12762858bea1ba7392928f284c3a33
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-20 16:23:00 +0300

    add documentation

--- CalculationsOnStrings/CalculationsOnStrings/CalculationsOnStrings.fs
+++ CalculationsOnStrings/CalculationsOnStrings/CalculationsOnStrings.fs
@@ -1,11 +1,15 @@
-﻿module CalculationsOnStrings
+﻿/// A module containing calculation on stings workflow.
+module CalculationsOnStrings
 
+/// A type representing calculation on strings builder.
 type CalculatorOnStringsBuilder() =
+    /// Binds.
     member this.Bind(x: string, f: int -> int option) =
         match System.Int32.TryParse x with
         | true, num -> f num
         | false, _ -> None
 
+    /// Returns.
     member this.Return(x: int) =
         Some x
 
