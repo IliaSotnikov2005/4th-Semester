c2379b79778c2793b4829b6026f01e34bc7fbb8e
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-21 23:52:39 +0300

    add ILAzy and SimpleLazy realisation

---
+++ Lazy/Lazy/ILazy.fs
@@ -0,0 +1,6 @@
+﻿namespace Lazy
+
+/// An interface for lazy.
+type ILazy<'a> =
+    /// Gets the value received by the supplier.
+    abstract member Get: unit -> 'a
---
+++ Lazy/Lazy/SimpleLazy.fs
@@ -0,0 +1,24 @@
+namespace Lazy
+
+type SimpleLazy<'a>(supplier: uint -> 'a) =
+    let mutable evaluatedValue = None
+    let mutable supplierOption = Some supplier
+
+    interface ILazy<'a> with
+        member this.Get () =
+            match evaluatedValue with
+            | Some (Ok value) -> value
+            | Some (Error ex) -> raise ex
+            | None ->
+                match supplierOption with
+                | Some supplier ->
+                    try
+                        let value = supplier ()
+                        evaluatedValue <- Some (Ok value)
+                        supplierOption <- None
+                        value
+                    with ex ->
+                        evaluatedValue <- Some (Error ex)
+                        supplierOption <- None
+                        raise ex
+                | None -> failwith "Supplier was already used"
