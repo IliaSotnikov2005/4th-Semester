5f493c3ee30316bc2a160d260876107ebb50cba3
Ilya Sotnikov <ilia12st@yandex.ru>
2025-04-22 15:04:58 +0300

    add lock free lazy and start writing tests

--- Lazy/Lazy.Tests/Tests.fs
+++ Lazy/Lazy.Tests/Tests.fs
@@ -1,8 +1,21 @@
 ﻿module Tests
 
-open System
-open Xunit
+open NUnit.Framework
+open Lazy
+open FsUnit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<Test>]
+let ``Single thread - simple supplier`` () =
+    let supplierCalls = ref 0
+    let supplier () = 
+        supplierCalls.Value <- supplierCalls.Value + 1
+        42
+       
+    let lazyInstance = SingleThreadLazy supplier :> ILazy<_>
+        
+    lazyInstance.Get() |> should equal 42
+    supplierCalls.Value |> should equal 1
+    
+    lazyInstance.Get() |> should equal 42
+    lazyInstance.Get() |> should equal 42
+    supplierCalls.Value |> should equal 1
---
+++ Lazy/Lazy/LockFreeLazy.fs
@@ -0,0 +1,29 @@
+namespace Lazy
+open System.Threading
+
+type LockFreeLazy<'a>(supplier: unit -> 'a) =
+    [<VolatileField>]
+    let mutable evaluatedValue = None
+    let mutable supplierOption = Some supplier
+
+    let tryEvaluate () =
+        let currentSupplier = Interlocked.Exchange(&supplierOption, None)
+        
+        match currentSupplier with
+        | None -> ()
+        | Some supplier ->
+            try
+                let value = supplier ()
+                Volatile.Write(&evaluatedValue, Some(Ok value))
+            with ex ->
+                Volatile.Write(&evaluatedValue, Some (Error ex))
+
+    interface ILazy<'a> with
+        member this.Get () =
+            tryEvaluate ()
+            let rec wait () =
+                match Volatile.Read(&evaluatedValue) with
+                | Some (Ok value) -> value
+                | Some (Error ex) -> raise ex
+                | None -> wait ()
+            wait ()
