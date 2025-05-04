fe93d1ecd7e6760f6156d023a52fc3f6933d027a
Ilya Sotnikov <ilia12st@yandex.ru>
2025-05-04 16:23:36 +0300

    update tests

--- ParenthesisSequence/ParenthesisSequence.Tests/Tests.fs
+++ ParenthesisSequence/ParenthesisSequence.Tests/Tests.fs
@@ -4,46 +4,29 @@ open Xunit
 open FsUnit.Xunit
 open ParenthesisSequence
 
-[<Fact>]
-let ``Empty string should be balanced`` () =
-    checkBracketBalance "" |> should be True
+let balancedCases = [
+    [| "" |]
+    [| "()" |]
+    [| "({[]})" |]
+    [| "(a{bdafdf}aac[dfdfeed]edfdfadf)" |]
+    [| "[]{{}()}({[()]})" |]
+]
 
-[<Fact>]
-let ``Single open bracket should be unbalanced`` () =
-    checkBracketBalance "(" |> should be False
+let unbalancedCases = [
+    [| "(" |]
+    [| ")" |]
+    [| "({[})" |]
+    [| "({[}])" |]
+    [| "(afsdaf{bd}cdddd[d}e)" |]
+    [| "[]{{}()}({[()]}))" |]
+]
 
-[<Fact>]
-let ``Single close bracket should be unbalanced`` () =
-    checkBracketBalance ")" |> should be False
+[<Theory>]
+[<MemberData(nameof balancedCases)>]
+let ``Should return true for balanced cases`` input =
+    checkBracketBalance input |> should be True
 
-[<Fact>]
-let ``Balanced parentheses should return true`` () =
-    checkBracketBalance "()" |> should be True
-
-[<Fact>]
-let ``Balanced mixed brackets should return true`` () =
-    checkBracketBalance "({[]})" |> should be True
-
-[<Fact>]
-let ``Unbalanced mixed brackets should return false`` () =
-    checkBracketBalance "({[})" |> should be False
-
-[<Fact>]
-let ``Nested unbalanced brackets should return false`` () =
-    checkBracketBalance "({[}])" |> should be False
-
-[<Fact>]
-let ``Balanced string with non-bracket characters should ignore them`` () =
-    checkBracketBalance "(a{bdafdf}aac[dfdfeed]edfdfadf)" |> should be True
-
-[<Fact>]
-let ``Unbalanced string with non-bracket characters should return false`` () =
-    checkBracketBalance "(afsdaf{bd}cdddd[d}e)" |> should be False
-
-[<Fact>]
-let ``Complex balanced string should return true`` () =
-    checkBracketBalance "[]{{}()}({[()]})" |> should be True
-
-[<Fact>]
-let ``Complex unbalanced string should return false`` () =
-    checkBracketBalance "[]{{}()}({[()]}))" |> should be False
+[<Theory>]
+[<MemberData(nameof unbalancedCases)>]
+let ``Should return false for unbalanced cases`` input =
+    checkBracketBalance input |> should be False
