1fa71bb901cc6c3aff429f757bb9df860b4e9b9b
Ilya Sotnikov <ilia12st@yandex.ru>
2025-06-06 12:09:51 +0300

    uplload tasks

---
+++ Test2/Test2.Tests/Tests.fs
@@ -0,0 +1,32 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+open FibonacciSum
+open Queue
+
+[<Test>]
+let ``Fibonacci sum should give answer`` () =
+    fibonacciEvenSum () |> should equal 1089154
+
+[<Test>]
+let ``queue works correct``() =
+    let queue = Queue<int>()
+    queue.Enqueue 1
+    queue.Enqueue 2
+    queue.Enqueue 3
+    queue.Enqueue 4
+
+    queue.Dequeue() |> should equal 1
+    
+    queue.Enqueue 6
+
+    queue.Dequeue() |> should equal 2
+    queue.Dequeue() |> should equal 3
+    queue.Dequeue() |> should equal 4
+    queue.Dequeue() |> should equal 6
+
+[<Test>]
+let ``queue throws exception if try to dequeue empty queue``() =
+    let queue = Queue<int>()
+    (fun () -> queue.Dequeue() |> ignore) |> should throw typeof<System.InvalidOperationException>
